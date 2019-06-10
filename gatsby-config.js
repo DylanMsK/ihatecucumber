75301b5414a17b131373ed558c8785504b8796cc
DylanMsK <kms920612@gmail.com>
2019-06-10 14:58:11 +0900

    edit algolia search query at 5 line, regex "//posts|pages//" to "//posts//"

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const config = require("./content/meta/config");
 
 const query = `{
-  allMarkdownRemark(filter: { id: { regex: "//posts|pages//" } }) {
+  allMarkdownRemark(filter: { id: { regex: "//posts//" } }) {
     edges {
       node {
         objectID: id
