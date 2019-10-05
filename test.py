ac7beba6c0a86bcef70ac68dc1accf703fc8c5c0
MINSU KANG <kms920612@gmail.com>
2019-10-05 16:54:38 +0900

    함수 2, 7-5까지 정리

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
-def add(x:int, y:int) -> int:
-    return x + y
-
-print(add.__annotations__)
+def test(a, b=None):
+    if not b:
+        print(f'b is {b}')
+    else:
+        print('yes')
+test(1)
+test(1, [])
+test(1, 0)
+test(1, '')
